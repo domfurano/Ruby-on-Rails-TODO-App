b7322e5cae39dd13d829549f10d2ec83a3b4a374
Furano <dominicfurano@gmail.com>
2014-04-23 13:20:59 -0600

    completed module 3 lesson 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   match "todos/index" => "todos#index", :as => :index, via: [:get, :post]
 
   match "todos/add" => "todos#add", :as => :add, via: [:get, :post]
 
   match "todos/delete" => "todos#delete", :as => :delete, via: [:get, :post]
+
+  match "todos/complete" => "todos#complete", :as => :complete, via: [:get, :post]
 
 end
