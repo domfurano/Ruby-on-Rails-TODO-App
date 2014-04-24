a32d5a54db135366e63b699de0c202292c11392e
Furano <dominicfurano@gmail.com>
2014-04-24 17:12:11 -0600

    fixed redirect issue

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,25 +7,6 @@ class TodosController < ApplicationController
 
 	def completed
 		@todo_items = Todo.all
-	end
-
-	def incomplete
-		@todo_items = Todo.all
-	end
-
-	def add
-		todo = Todo.create(:todo_item => params[:todo][:todo_item])
-		if !todo.valid?
-			flash[:error] = todo.errors.full_messages.join("<br\>").html_safe
-		else
-			todo.update_attribute(:completed, false)
-			flash[:success] = "todo added"
-		end
-		redirect_to :action => 'complete'
-	end
-
-	def complete
-		# Check to see if any boxes are checked.
 		if !params[:todos_checkbox].nil?
 			params[:todos_checkbox].each do |check|
 				todo_id = check
@@ -41,10 +22,45 @@ class TodosController < ApplicationController
 					else
 						t.update_attribute(:completed, false)
 					end
-			  end
+				end
 			end
 		end
-	redirect_to :action => 'index'
+	end
+
+	def incomplete
+		@todo_items = Todo.all
+		if !params[:todos_checkbox].nil?
+			params[:todos_checkbox].each do |check|
+				todo_id = check
+				t = Todo.find_by_id(todo_id)
+				# Had to combine delete button action here.
+				if params[:delete_button]
+					t.delete
+					t.save
+				# Else the complete button was selected.
+				else
+					if t.read_attribute(:completed) == false
+						t.update_attribute(:completed, true)
+					else
+						t.update_attribute(:completed, false)
+					end
+				end
+			end
+		end
+	end
+
+	def add
+		todo = Todo.create(:todo_item => params[:todo][:todo_item])
+		if !todo.valid?
+			flash[:error] = todo.errors.full_messages.join("<br\>").html_safe
+		else
+			todo.update_attribute(:completed, false)
+			flash[:success] = "todo added"
+		end
+		redirect_to :action => 'index'
+	end
+
+	def complete
 	end
 
 end
