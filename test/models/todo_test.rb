766d21819deb148c5251153cba4616879645d909
Michael Smith <michael.christian.smith@gmail.com>
2014-04-25 14:15:13 -0600

    created uber-basic tests

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "the truth" do
+		assert true
+	end
+	
+	test "can't save a blank todo" do
+	t = Todo.new
+	assert_not t.save, "Created a blank todo"
+	end
 end
